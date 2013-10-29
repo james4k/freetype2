92a9ca211f265c9faab913c46cde133f198def0d
James Gray <james@james4k.com>
2013-10-28 21:44:57 -0500

    added package docs

--- freetype.go
+++ freetype.go
@@ -1,3 +1,21 @@
+/*
+Package freetype2 is a minimal wrapper for the FreeType 2 library, with an API
+focused around scalable vector fonts.
+
+"FreeType is a software font engine that is designed to be small, efficient,
+highly customizable, and portable while capable of producing high-quality
+output (glyph images). It can be used in graphics libraries, display servers,
+font conversion tools, text image generation tools, and many other products as
+well.
+
+Note that FreeType is a font service and doesn't provide APIs to perform
+higher-level features like text layout or graphics processing (e.g., colored
+text rendering, ‘hollowing’, etc.). However, it greatly simplifies these tasks
+by providing a simple, easy to use, and uniform interface to access the content
+of font files."
+
+Visit http://freetype.org for more on FreeType 2.
+*/
 package freetype2
 
 /*
